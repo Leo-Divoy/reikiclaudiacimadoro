b7318ba479338b2228432dcad9d862d28d3b139b
Leonel Divoy <leodivoy@hotmail.com>
2020-10-09 19:33:35 -0300

    Arreglado email de contacto

--- enviar.php
+++ enviar.php
@@ -1,10 +1,9 @@
 <?php
-$destino="info@reiki.com.ar";
+$destino="clauc66@hotmail.com";
 $nombre= $_POST ["nombre"];
 $correo= $_POST ["correo"];
 $asunto= $_POST ["asunto"];
 $mensaje= $_POST ["mensaje"];
 $contenido= "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje: " . $mensaje;
 mail ($destino, $asunto, $contenido);
-header ("Location: gracias.html");
 ?>
